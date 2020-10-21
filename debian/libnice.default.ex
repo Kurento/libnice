8b6fc53d61b56c3e63e465b2b157b6776516f524
Juan Navarro <juan.navarro@gmx.es>
2020-10-21 12:20:32 +0200

    Initial debian files

---
+++ debian/libnice.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libnice initscript
+# sourced by /etc/init.d/libnice
+# installed at /etc/default/libnice by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
